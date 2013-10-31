9e26f6e7052332ece347b6414315a1f4fe02fb2e
JCT250 <james.taylor250@gmail.com>
2013-10-31 14:01:11 +1300

    worked on RX to work with new controller

--- MowerRX/MowerRX.ino
+++ MowerRX/MowerRX.ino
@@ -9,67 +9,52 @@ unsigned long inTime = 0;
 byte inData;
 byte inArray[4];
 
-static int left_ALI = 0; //pwm inputs on H-bridge
-static int left_BLI = 0;
-static int right_ALI = 0;
-static int right_BLI = 0;
-static int mosfet1 = 0; //powers 12v reg
-static int mosfet2 = 0; //powers relay high side
-static int BJT1 = 0; //switches relay low side
-static int BJT2 = 0; //switches brake BJT
-static int H_Enable = 0; // enable pins on H-Bridge
+static int left_speed_output = 0;
+static int left_direction_output = 0;
+static int right_speed_output = 0;
+static int right_direction_output = 0;
+static int brakes = 0;
 
 int left_speed = 0;
 int left_direction = 0;
+int left_direction_old = 0;
 int right_speed = 0;
 int right_direction = 0;
+int right_direction_old = 0;
 volatile int handbrake = 1;
 
 void setup()
 {
 	Serial.begin(9600);
-	
-	pinMode(left_ALI, OUTPUT);
-	pinMode(left_ALI, OUTPUT);
-	pinMode(right_ALI, OUTPUT);
-	pinMode(right_BLI, OUTPUT);
-	pinMode(mosfet1, OUTPUT);
-	pinMode(mosfet2, OUTPUT);
-	pinMode(BJT1, OUTPUT);
-	pinMode(BJT1, OUTPUT);
-	pinMode(H_Enable, OUTPUT);
 
-
-  /* add setup code here */
 
 }
 
 void serial_check()		//Checks for new data in the serial buffer and if available stores in a string
 {
 	int i = 0;
-	inData = 0xFF;
+	inData = 0x7F; // fill the array with a value that represents a 0 throttle value (just in case there are problems reading the serial string and odd values are ending up in the array)
 	if(Serial.available() > 3){ //find the start byte
 		while(inData != start_byte){ 
 			inData = Serial.read();
 		} 
-		inArray[i] = inData;
-		i++;
-		while(inData != end_byte){ //then read data into the incoming array until you reach an end byte
-			inData = Serial.read();
-			inArray[i] = inData;
-			i++; //move to next position in the incoming array
-		}
+		inArray[0] = inData;
+		inArray[1] = Serial.read();
+		inArray[2] = Serial.read();
 	
-		for(i=0; i++; i<5){ //echo the array to the serial port for troubleshooting
+		for(i=0; i++; i<4){ //echo the array to the serial port for troubleshooting
 			Serial.print(inArray[i]);
 			Serial.print(" ");
 		}
+		Serial.println();
+
 		inTime = millis(); //set the time that checking and receiving finished
+
 		process = true;
 	}
 }
 
-void serial_process() //process serial string to give a pwm value ready to send to motor driver board
+void serial_process() //process serial string to give a value ready to send to motor controller
 { 
 	left_speed = 0;
 	right_speed = 0;
@@ -78,7 +63,7 @@ void serial_process() //process serial string to give a pwm value ready to send 
 	{
 		handbrake = 0;
 		left_direction = 0;
-		left_speed = map(inArray[1], 0,126,0,255);
+		left_speed = map(inArray[1], 0,126,255,0);
 	}
 	if(inArray[1] > 128)
 	{
@@ -90,7 +75,7 @@ void serial_process() //process serial string to give a pwm value ready to send 
 	{
 		handbrake = 0;
 		right_direction = 0;
-		right_speed = map(inArray[2], 0,126,0,255);
+		right_speed = map(inArray[2], 0,126,255,0);
 	}
 	if(inArray[2] > 128)
 	{
@@ -98,33 +83,37 @@ void serial_process() //process serial string to give a pwm value ready to send 
 		right_direction = 1;
 		right_speed = map(inArray[2], 129,255,0,255);
 	}
+	drive = true;
 }
 
 void drive_motors() //drives the motors
 {
 	if(handbrake == 0){
-		digitalWrite(BJT2, HIGH); //disengage brakes
-		digitalWrite(H_Enable, LOW); //enable outputs on H-Bridge
-		if(left_direction == 0) //if left reverse
-		{
-			analogWrite(left_ALI, 0);
-			analogWrite(left_BLI, left_speed);
-		}
-		if(left_direction == 1) // if left foreward
-		{
-			analogWrite(left_ALI, left_speed);
-			analogWrite(left_BLI, 0);
-		}
-		if(right_direction == 0) // if right reverse
-		{
-			analogWrite(right_ALI, 0);
-			analogWrite(right_BLI, right_speed);
-		}
-		if(right_direction == 1) //if right foreward
-		{
-			analogWrite(right_ALI, right_speed);
-			analogWrite(right_BLI, 0);
-		}
+			
+			digitalWrite(brakes, HIGH); //make sure brakes are off
+			
+			if(left_direction != left_direction_old) //if we need to change direction 
+			{
+				analogWrite(left_speed_output, 0); //stop the bot
+				delay(10); //see what happens with this, we may not need it
+				digitalWrite(left_direction_output, left_direction); //set the new direction
+				left_direction_old = left_direction; // update the stored value
+				delay(10); //ditto with this one, may not be needed
+			}
+
+			analogWrite(left_speed_output, left_speed); //write the new speed to the controller
+		
+			if(right_direction != right_direction_old)
+			{
+				analogWrite(right_speed_output, 0);
+				delay(10);
+				digitalWrite(right_direction_output, right_direction);
+				right_direction_old = right_direction;
+				delay(10);
+			}
+
+			analogWrite(right_speed_output, right_speed);
+		
 	}
 	if(handbrake == 1)
 	{
@@ -134,12 +123,12 @@ void drive_motors() //drives the motors
 
 void stop()
 {
-	digitalWrite(H_Enable, HIGH); //disable the outputs on the H-Bridge
-	analogWrite(left_ALI, 0); //write 0 to PWM inputs
-	analogWrite(left_BLI, 0);
-	analogWrite(right_ALI, 0);
-	analogWrite(right_BLI, 0);
-	digitalWrite(BJT2, LOW); //apply the motor brakes
+	
+	analogWrite(left_speed_output, 0);
+	analogWrite(right_speed_output, 0);
+	digitalWrite(brakes, LOW);
+
+
 }
 
 void interrupt()
